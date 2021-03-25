70a01f42e579a19b0ed34a219942c077ead256b5
Konrad Droeske <kdroeske@gmail.com>
2021-03-25 16:40:16 -0400

    changed app text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Updated app.
+          I was changed on the feature branch.
         </a>
       </header>
     </div>
